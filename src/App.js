19129b3c0fce344354404fd874f2fd928c40a502
Han-Saem Park <samiparken@gmail.com>
2020-10-15 16:50:19 +0200

    Apply Expressions & Conditionals In JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
 import "./App.css";
 
+//JSX
 class App extends Component {
+  foos = () => "Bars";
+
   render() {
-    return React.createElement(
-      "div",
-      { className: "App" },
-      React.createElement("h1", null, "Hello from React")
+    const name = "John Doe";
+    const foo = () => "Bar";
+    const loading = false;
+    const showName = true;
+
+    // if (loading) {
+    //   return <h4>Loading...</h4>;
+    // }
+
+    return (
+      <div className='App'>
+        <h1>My First Page</h1>
+        {loading ? <h4>Loading...</h4> : <h4> DONE!</h4>}
+        <h2> Hello {showName ? name : null} </h2>
+        <h2> Hello {showName && name} </h2> {/*Alt Tenary Operation*/}
+        <h2> toUppercase() : {name.toUpperCase()} </h2>
+        <h2> 2 + 2 = {2 + 2} </h2>
+        <h2> foo = {foo()} </h2>
+        <h2> foos = {this.foos()}</h2>
+      </div>
     );
   }
 }
