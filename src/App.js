322eff6f9320e8896a211a2c4502198f78750992
Han-Saem Park <samiparken@gmail.com>
2020-10-15 16:20:35 +0200

    Apply Fragment without JSX

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Fragment, Component } from "react";
+import "./App.css";
 
-
+// without JSX
 class App extends Component {
   render() {
-
-      return (
-    <div className="App">
-      <h1> Hello World! from React </h1>
-    </div>
-  );
-
+    return (
+      <>
+        <h1> Valkommen till React </h1>
+        <label htmlFor='name'>Name ?</label>
+        <h2> Hejda! </h2>
+      </>
+    );
   }
 }
 
