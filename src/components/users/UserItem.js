aaf69c68916af9c19599a82b2e5dec135802fa03
Han-Saem Park <samiparken@gmail.com>
2020-10-16 13:44:35 +0200

    Try Lists & Passing State with Props

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
 
 export class UserItem extends Component {
-  state = {
-    id: "id",
-    login: "mojombo",
-    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-    html_url: "https://github.com/mojombo",
-  };
-
   render() {
-    const { login, avatar_url, html_url } = this.state;
+    const { login, avatar_url, html_url } = this.props.user;
 
     return (
       <div className='card text-center'>
@@ -20,7 +13,6 @@ export class UserItem extends Component {
           style={{ width: "60px" }}
         />
         <h3> {login} </h3>
-
         <div>
           <a href={html_url} className='btn btn-dark btn-sm my-1'>
             More
