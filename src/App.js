e00ab2126875b9767d64f29ad1d2a532f010ab42
Han-Saem Park <samiparken@gmail.com>
2020-10-16 15:03:33 +0200

    Try async componentDidMount()

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  async componentDidMount() {
+    const res = await axios.get("https://api.github.com/users");
+
+    console.log(res.data);
+  }
+
   render() {
     return (
       <div className='App'>
