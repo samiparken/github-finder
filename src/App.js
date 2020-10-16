a0a3ee319b6d17a603156c8d545c58c59e1308d2
Han-Saem Park <samiparken@gmail.com>
2020-10-16 11:12:37 +0200

    Try Component State 1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/NavBar";
+import UserItem from "./components/users/UserItem";
 import "./App.css";
 
-//JSX
 class App extends Component {
-  foos = () => "Bars";
-
   render() {
-    const name = "Han-Saem Park";
-    const foo = () => "Bar";
-    const loading = false;
-    const showName = true;
-    const numbers = [1, 2, 3, 4];
-
-    // if (loading) {
-    //   return <h4>Loading...</h4>;
-    // }
-
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
-        <Navbar title={numbers} />
-        {/*other Component*/}
-        <h1>My First Page</h1>
-        {loading ? <h4>Loading...</h4> : <h4> DONE! </h4>}
-        <h2> Hello {showName ? name : null} </h2>
-        <h2> Hello {showName && name} </h2> {/*Alt Ternary Operation*/}
-        <h2> toUppercase() : {name.toUpperCase()} </h2>
-        <h2> 2 + 2 = {2 + 2} </h2>
-        <h2> foo = {foo()} </h2>
-        <h2> foos = {this.foos()}</h2>
+        <UserItem />
       </div>
     );
   }
