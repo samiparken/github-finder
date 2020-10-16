f06ed39db30a0d386e8941e6738c14f5827e14e4
Han-Saem Park <samiparken@gmail.com>
2020-10-16 10:08:34 +0200

    Try passing Props & Default Props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/NavBar";
 import "./App.css";
 
 //JSX
@@ -6,7 +7,7 @@ class App extends Component {
   foos = () => "Bars";
 
   render() {
-    const name = "John Doe";
+    const name = "Han-Saem Park";
     const foo = () => "Bar";
     const loading = false;
     const showName = true;
@@ -17,10 +18,12 @@ class App extends Component {
 
     return (
       <div className='App'>
+        <Navbar title='Github Finder' icon='fab fa-github' />{" "}
+        {/*other Component*/}
         <h1>My First Page</h1>
-        {loading ? <h4>Loading...</h4> : <h4> DONE!</h4>}
+        {loading ? <h4>Loading...</h4> : <h4> DONE! </h4>}
         <h2> Hello {showName ? name : null} </h2>
-        <h2> Hello {showName && name} </h2> {/*Alt Tenary Operation*/}
+        <h2> Hello {showName && name} </h2> {/*Alt Ternary Operation*/}
         <h2> toUppercase() : {name.toUpperCase()} </h2>
         <h2> 2 + 2 = {2 + 2} </h2>
         <h2> foo = {foo()} </h2>
---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,22 @@
+// Under Global CSS
+import React, { Component } from "react";
+
+export class NavBar extends Component {
+  static defaultProps = {
+    title: "Github Finder",
+    icon: "fab fa-github",
+  };
+
+  render() {
+    return (
+      <nav className='navbar bg-primary'>
+        <h1>
+          <i className={this.props.icon} /> {this.props.title}{" "}
+          {/*Passing props */}
+        </h1>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
