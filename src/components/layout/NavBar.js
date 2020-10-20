339a6765d93d22f311b1371ae5a1fc17ee17dc20
Han-Saem Park <samiparken@gmail.com>
2020-10-20 10:43:13 +0200

    Add NavBar Buttons (Home/About)

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -9,6 +9,14 @@ const Navbar = ({ icon, title }) => {
       <h1>
         <i className={icon} /> {title}
       </h1>
+      <ul>
+        <li>
+          <a href='/'>Home</a>
+        </li>
+        <li>
+          <a href='/about'>About</a>
+        </li>
+      </ul>
     </nav>
   );
 };
