37e8f35a4442cb794918771bb25dc4446acb23ee
Han-Saem Park <samiparken@gmail.com>
2020-10-20 11:23:07 +0200

    Add spinner when getting Single User page

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+  };
 
   render() {
     const {
@@ -22,6 +30,7 @@ export class User extends Component {
     } = this.props.user;
 
     const { loading } = this.props;
+    if (loading) return <Spinner />;
 
     return <div>{name}</div>;
   }
