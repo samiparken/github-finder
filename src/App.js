cf357cd48c9d2832aff8698301bc3a2ab2be7d72
Han-Saem Park <samiparken@gmail.com>
2020-10-21 15:09:48 +0200

    Merge Search & Users Components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; // for organizing pages with address
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
-import Search from "./components/users/Search";
-import Users from "./components/users/Users";
+import Home from "./components/pages/Home";
 import User from "./components/users/User";
 import About from "./components/pages/About";
 
@@ -23,17 +22,7 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
-
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
               </Switch>
