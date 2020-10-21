8af021b29b936ae05be47245f8b36c6e1a7c321c
Han-Saem Park <samiparken@gmail.com>
2020-10-21 15:14:02 +0200

    Add NotFound component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; // for organizing pages with address
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
 import Home from "./components/pages/Home";
 import User from "./components/users/User";
 import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 
 // State
 import GithubState from "./context/github/GithubState";
@@ -25,6 +26,7 @@ const App = () => {
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
